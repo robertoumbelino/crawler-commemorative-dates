a0d9b43d66d4c6f132c575614f2db1d9499c630d
Roberto Umbelino <beto.umbelino@gmail.com>
2021-09-23 19:46:01 -0300

    feat: added crawler to get commemorations

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,83 @@
+import * as Crawler from 'crawler'
+
 import { client } from './db/prisma'
 
-const run = async () => {
+const months = [
+  'janeiro',
+  'fevereiro',
+  'marco',
+  'abril',
+  'maio',
+  'junho',
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro'
+]
+
+const crawler = new Crawler()
+
+const saveCommemorationsFromMonth = async (monthIndex = 0) => {
+  const month = months[monthIndex]
+
+  if (!month) return
+
+  console.log(month)
+
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
   try {
-    // client.commemorativeDate.findMany().then(dates => console.log(dates))
-    // const createdDate = await client.commemorativeDate.create({
-    //   data: {
-    //     day: 23,
-    //     month: 9,
-    //     title: 'Daleeee'
-    //   }
-    // })
-    // console.log(createdDate)
+    crawler.queue({
+      url: 'https://www.guialince.com.br/datas-comemorativas/janeiro',
+      callback: async (error, res, done) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+
+        const $ = res.$
+        const promises = $('table.table-hover > tbody > tr')
+          .toArray()
+          .map(async line => {
+            const day = +$(line).find('td:nth-child(1)').text()
+            const commemoration = $(line).find('td:nth-child(2)').text()
+
+            const foundDate = await client.commemorativeDate.findFirst({
+              where: {
+                day,
+                month: monthIndex + 1,
+                title: commemoration
+              }
+            })
+
+            if (foundDate) {
+              console.log('Já existe', foundDate)
+              return
+            }
+
+            const createdDate = await client.commemorativeDate.create({
+              data: {
+                day,
+                month: monthIndex + 1,
+                title: commemoration
+              }
+            })
+
+            console.log(createdDate)
+          })
+
+        await Promise.all(promises)
+
+        done()
+
+        await saveCommemorationsFromMonth(monthIndex + 1)
+      }
+    })
   } catch ({ message }) {
     console.error(message)
   }
 }
 
-run()
+saveCommemorationsFromMonth()
