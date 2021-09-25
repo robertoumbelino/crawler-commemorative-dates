01ed473ef3053cb1e1ca37bb23ceedebbe7cc0f8
Roberto Umbelino <beto.umbelino@gmail.com>
2021-09-24 23:19:46 -0300

    feat: improved code base

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import * as Crawler from 'crawler'
 
 import { client } from './db/prisma'
 
-const months = [
+/**
+ * Months in portuguese, to be used as parameter in destination page.
+ */
+const MONTHS = [
   'janeiro',
   'fevereiro',
   'marco',
@@ -17,67 +20,114 @@ const months = [
   'dezembro'
 ]
 
+/**
+ * Destination page.
+ */
+const BASE_URL = 'https://www.guialince.com.br/datas-comemorativas'
+
+/**
+ * Crawler instance.
+ */
 const crawler = new Crawler()
 
+/**
+ * Recursively function to get commemorative dates.
+ */
 const saveCommemorationsFromMonth = async (monthIndex = 0) => {
-  const month = months[monthIndex]
+  /**
+   * Get month name.
+   */
+  const month = MONTHS[monthIndex]
 
+  /**
+   * Invalid month, finish it.
+   */
   if (!month) return
 
-  console.log(month)
-
-  await new Promise(resolve => setTimeout(resolve, 2000))
+  console.log(`Getting commemorative dates from ${month}`)
 
   try {
+    /**
+     * URL from destination page.
+     */
+    const url = `${BASE_URL}/${month}`
+
+    /**
+     * Make request.
+     */
     crawler.queue({
-      url: `https://www.guialince.com.br/datas-comemorativas/${month}`,
+      url,
       callback: async (error, res, done) => {
+        /**
+         * Stop process when has an error.
+         */
         if (error) {
-          console.error(error)
+          console.error('An error occurred', error)
           return
         }
 
+        /**
+         * Get jQuery.
+         */
         const $ = res.$
-        const promises = $('table.table-hover > tbody > tr')
-          .toArray()
-          .map(async line => {
-            const day = +$(line).find('td:nth-child(1)').text()
-            const commemoration = $(line).find('td:nth-child(2)').text()
 
-            const foundDate = await client.commemorativeDate.findFirst({
-              where: {
-                day,
-                month: monthIndex + 1,
-                title: commemoration
-              }
-            })
+        /**
+         * Get elements with commemorative dates.
+         */
+        const elements = $('table.table-hover > tbody > tr').toArray()
 
-            if (foundDate) {
-              console.log('Já existe', foundDate)
-              return
-            }
+        /**
+         * Save commemorative dates.
+         */
+        const promises = elements.map(async line => {
+          /**
+           * Get data.
+           */
+          const day = +$(line).find('td:nth-child(1)').text()
+          const commemoration = $(line).find('td:nth-child(2)').text()
 
-            const createdDate = await client.commemorativeDate.create({
-              data: {
-                day,
-                month: monthIndex + 1,
-                title: commemoration
-              }
-            })
+          /**
+           * Payload to save in database.
+           */
+          const data = { day, month: monthIndex + 1, title: commemoration }
 
-            console.log(createdDate)
-          })
+          /**
+           * Find to check if already exists.
+           */
+          const filter = { where: data }
+          const foundDate = await client.commemorativeDate.findFirst(filter)
+
+          if (foundDate) {
+            console.log('Commemorative date already exists', foundDate)
+            return
+          }
+
+          /**
+           * Create in database.
+           */
+          const createdDate = await client.commemorativeDate.create({ data })
+
+          console.log('Saved commemorative date', createdDate)
+        })
 
         await Promise.all(promises)
 
         done()
-
-        await saveCommemorationsFromMonth(monthIndex + 1)
       }
     })
   } catch ({ message }) {
     console.error(message)
+  } finally {
+    /**
+     * Sleep to don't make many requests in destination page.
+     */
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    await saveCommemorationsFromMonth(monthIndex + 1)
   }
 }
 
+/**
+ * Start.
+ */
 saveCommemorationsFromMonth()
